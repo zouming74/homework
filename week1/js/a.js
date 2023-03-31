3e3005a799c63ab4e9b3fdb83023caabb38fcdcf
zouming74 <1071203580@qq.com>
2023-03-31 11:33:34 +0800

    repo init

---
+++ week1/js/a.js
@@ -0,0 +1,3 @@
+window.onload = function() {
+            alert('你好，同学！')
+        }
---
+++ week5/js/0327.js
@@ -0,0 +1,45 @@
+window.onload = function(){
+    // alert("页面加载完成")
+}
+// function colorChag(value){
+//     const ele = document.getElementById("lb1"); //驼峰命名
+//     ele.style.color = value;
+//     alert("颜色改变"); //body
+// }
+
+function colorChag(value){
+    const eleA = document.getElementsByClassName("lb");
+    for(var i = eleA.length -1;i>= 0;i--){
+        eleA[i].style.color = value;
+    }
+}
+
+function rangeChag(arg){
+    const eleA = document.getElementsByClassName("container");
+    let arg1 = 255-arg;
+    eleA[0].style.backgroundColor = "rgb("+arg1+","+arg1+","+arg1+")";
+}
+
+function checkPid(pid){
+    const baseFac = new Array(7,9,10,5,8,4,2,1,6,3,7,9,10,5,8,4,2)
+    const checkA = new Array('0','1','X','9','8','7','6','5','4','3','2')
+    const eleAlarm = document.getElementsByClassName("alarm");
+    const ele = document.getElementById("ip2")
+
+    const factor = pid.split('');
+    let sum = 0;
+    for(var i = 0;i<17;i++)
+    {
+        sum += factor[i]*baseFac[i];
+
+    }
+    if(checkA[sum % 11] != factor[17])
+    {
+        ele.style.borderColor = "red";
+        eleAlarm[0].style.display = "inline";
+    }
+    else{
+        ele.style.borderColor = "gray";
+        eleAlarm[0].style.display = "none";
+    }
+}
