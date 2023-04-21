687ff87e702e52e0deda9137780f490ef66b1095
zouming74 <1071203580@qq.com>
2023-04-21 23:12:19 +0800

    feat(/homework):create bar feature create bar feature and finish feedback functionally

---
+++ homework/js/homework1.js
@@ -0,0 +1,10 @@
+function dispCata(){
+    var oDiv = document.getElementById('catalogs');
+    if(oDiv.style.display == 'none')
+    {
+        oDiv.style.display = 'block';
+     }
+     else{
+        oDiv.style.display = 'none';
+    } 
+}
