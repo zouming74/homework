a3fe40d02d308a1bba9c7bfba26ea69914258996
zouming74 <1071203580@qq.com>
2023-05-24 22:54:00 +0800

    feat(/homework): feat horizontal slide turn
    Fixed the mistakes about horizontal turn

--- homework/js/slides.js
+++ homework/js/slides.js
@@ -1,96 +1,75 @@
-// window.onload = function(){
-// 	let aele = document.getElementsByClassName("menuitem");
-
-// 	aele[0].style.backgroundColor = "red";
-// }
-// function move(arg){
-// 	let aele = document.getElementsByClassName("menuitem");
-// 	clear(aele);
-// 	aele[arg].style.backgroundColor = "red";
-
-// 	let aMove = document.getElementsByClassName("move");
-// 	aMove[0].style.top = - arg * 100 + "%";  ///运算完后是100%；
-// }
-// function clear(aelement){
-// 	for (var i = 0;i < aelement.length; i++) {
-// 		aelement[i].style.backgroundColor = "blue";
-// 	}
-// }
-
-window.onload=function(){
-    var slides=document.getElementsByClassName("slide");
-    var leftBtn=document.getElementsByClassName("btn-left");
-    var rightBtn=document.getElementsByClassName("btn-right");
-    var menuitems=document.getElementsByClassName(".menu.menuitem");
-    var container=document.getElementsByClassName("container");
+window.onload = function() {
+    var slides = document.getElementsByClassName("slide");
+    var leftBtn = document.getElementsByClassName("btn-left")[0];
+    var rightBtn = document.getElementsByClassName("btn-right")[0];
+    var menuitems = document.getElementsByClassName("menuitem");
+    var container = document.getElementsByClassName("container")[0];
     var index = 0;
-    var timer=null;
+    var timer = null;
 
-    //清除class
-    var clearClass=function(){
-        for(let i=0;i<slides.length;i++){
-            slides[i].className="slide";
-            menuitems[i].className="menuitem";
-            menuitems[i].setAttribute("num",i);
+    // 清除class
+    var clearClass = function() {
+        for (let i = 0; i < slides.length; i++) {
+            slides[i].className = "slide";
+            menuitems[i].className = "menuitem";
+            menuitems[i].setAttribute("num", i);
         }
     }
 
-    //只显示一个slide
-    function move(){
+    // 只显示一个slide
+    function move() {
         clearClass();
-        slides[index].className="slide active";
-        menuitems[index].className="menuitem red";
+        slides[index].className = "slide active";
+        menuitems[index].className = "menuitem red";
+        // menuitems[index].getElementsByClassName.background = "red";
     }
 
-    //向右切换
-    rightBtn.onclick=function(){
-        if(index<slides.length-1){
+    // 向右切换
+    rightBtn.onclick = function() {
+        if (index < slides.length - 1) {
             index++;
-        }
-        else{
-            index=0;
-        }
-        move();
-    }
-    
-    //向左切换
-    rightBtn.onclick=function(){
-        if(index<slides.length){
-            index--;
-        }
-        else{
-            index=slides.length-1;
+        } else {
+            index = 0;
         }
         move();
     }
 
-    //定时器，实现轮播
-    timer=setInterval(function(){
+    // 向左切换
+    leftBtn.onclick = function() {
+        if (index > 0) {
+            index--;
+        } else {
+            index = slides.length - 1;
+        }
+        move();
+    }
+
+    // 定时器，实现轮播
+    timer = setInterval(function() {
         rightBtn.onclick();
-    },1500)
+    }, 1500);
 
     // 点击数字切换
-    for(var i=0;i<slides.length;i++){
-        menuitems[i].addEventListener("click",function(){
-            var point_index=this.getAttribute("num");
-            index=point_index;
+    for (var i = 0; i < slides.length; i++) {
+        menuitems[i].addEventListener("click", function() {
+            var point_index = this.getAttribute("num");
+            index = point_index;
             move();
-        })
+        });
     }
 
-    //鼠标移入和鼠标移出
-    container.onmouseover=function(){
+    // 鼠标移入和鼠标移出
+    container.onmouseover =  function() {
         clearInterval(timer);
-        timer=setInterval(function(){
+        timer = setInterval(function() {
             rightBtn.onclick();
-        },1500)
+        }, 6000);
     }
-    container.onmouseleave=function(){
+
+    container.onmouseleave = function() {
         clearInterval(timer);
-        timer=setInterval(function(){
+        timer = setInterval(function() {
             rightBtn.onclick();
-        },1500)
+        }, 3000);
     }
-
-
 }
