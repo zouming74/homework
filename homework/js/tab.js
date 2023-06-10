d5a815081907eada0abdbb1b15f0437ac376fc74
zouming74 <1071203580@qq.com>
2023-06-10 19:33:56 +0800

    docs(/homework):create tab
    using tab-tech builds a website

---
+++ homework/js/tab.js
@@ -0,0 +1,29 @@
+window.onload = function(argument){
+	// alert('ee')
+	let el = document.querySelector(".menu a");
+	el.style.color = "red";
+	let el1 = document.querySelector(".page a");
+	el1.style.color = "blue";
+}
+function doSwitch (arg){
+	ini();
+
+    let ele = document.querySelector(".menu a:nth-of-type(" + (arg + 1) + ")");
+    ele.style.background = "white";
+    ele.style.borderBottom = "none";
+
+    let arr = document.getElementsByClassName("page");
+    arr[arg].style.display = "block";
+}
+function ini(){
+    let arr = document.getElementsByClassName("page");
+
+	for (var i = 0; i < 5; i++) {
+		let ele = document.querySelector(".menu a:nth-of-type(" + (i + 1) + ")");
+        
+        ele.style.background="#eee";
+        ele.style.borderBottom="1px solid #707070";
+
+        arr[i].style.display = "none";
+	}
+}
