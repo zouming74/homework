5e8d2e493148943727cee38bc044b247abc21fba
zouming74 <1071203580@qq.com>
2023-06-15 15:49:29 +0800

    docs(/homework):rewrite slides function

--- homework/js/slides.js
+++ homework/js/slides.js
@@ -1,11 +1,22 @@
 window.onload = function() {
-    var slides = document.getElementsByClassName("slide");
-    var leftBtn = document.getElementsByClassName("btn-left")[0];
-    var rightBtn = document.getElementsByClassName("btn-right")[0];
-    var menuitems = document.getElementsByClassName("menuitem");
-    var container = document.getElementsByClassName("container")[0];
+    // var slides = document.getElementsByClassName("slide");
+    // var leftBtn = document.getElementsByClassName("btn-left")[0];
+    // var rightBtn = document.getElementsByClassName("btn-right")[0];
+     var menuitems = document.getElementsByClassName("menuitem"); 
+    // var container = document.getElementsByClassName("container")[0];
     var index = 0;
-    var timer = null;
+     var timer = null;
+
+    //使用到的对象
+    let container = document.querySelector(".container");
+    let move = document.querySelector(".move");
+    let slides = document.querySelectorAll(".slide");
+    let left = document.querySelector(".btn-left");
+    let right = document.querySelector(".btn-right");
+
+    //初始化变量
+    let curr = 9;
+
 
     // 清除class
     var clearClass = function() {
@@ -17,36 +28,47 @@ window.onload = function() {
     }
 
     // 只显示一个slide
-    function move() {
+    function mov() {
         clearClass();
         slides[index].className = "slide active";
         menuitems[index].className = "menuitem red";
-        // menuitems[index].getElementsByClassName.background = "red";
     }
 
     // 向右切换
-    rightBtn.onclick = function() {
-        if (index < slides.length - 1) {
-            index++;
-        } else {
-            index = 0;
+    right.onclick = function() {
+        // if (index < slides.length - 1) {
+        //     index++;
+        // } else {
+        //     index = 0;
+        // }
+        curr++;
+        move.style.left = -curr*980 + "px";
+        if(curr>=slides.length){
+            curr = 0;
+            move.style.left = "0px";
         }
-        move();
+        mov();
     }
 
     // 向左切换
-    leftBtn.onclick = function() {
-        if (index > 0) {
-            index--;
-        } else {
-            index = slides.length - 1;
+    left.onclick = function() {
+        // if (index > 0) {
+        //     index--;
+        // } else {
+        //     index = slides.length - 1;
+        // }
+        curr--;
+        move.style.left = -curr*980 + "px";
+        if(curr<0){
+            curr = slides.length -1;
+            move.style.left = "-3920px"
         }
-        move();
+        mov();
     }
 
     // 定时器，实现轮播
     timer = setInterval(function() {
-        rightBtn.onclick();
+        right.onclick();
     }, 1500);
 
     // 点击数字切换
@@ -54,7 +76,7 @@ window.onload = function() {
         menuitems[i].addEventListener("click", function() {
             var point_index = this.getAttribute("num");
             index = point_index;
-            move();
+            mov();
         });
     }
 
@@ -62,13 +84,13 @@ window.onload = function() {
     container.onmouseover =  function() {
         clearInterval(timer);
         timer = setInterval(function() {
-            rightBtn.onclick();
+            right.onclick();
         }, 60000);
     }
     container.onmouseleave = function() {
         clearInterval(timer);
         timer = setInterval(function() {
-            rightBtn.onclick();
+            right.onclick();
         }, 3000);
     }
     // cd player
